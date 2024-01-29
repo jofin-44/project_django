99ca179d7599642ce5ba8e3154ab0c9d24caef11
jofin-44 <jofin.siby@msds.christuniversity.in>
2024-01-29 13:42:52 +0530

    home update

--- sportsmanagement/urls.py
+++ sportsmanagement/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from student.views import index
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
